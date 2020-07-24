04582b84634af0b82179aa5ff78ebfea77959c56
Maribelle Leong <maribelle.leong@gmail.com>
2020-07-24 17:42:34 -0300

    created a simple Navbar component

--- client/src/components/Layout/Navbar.js
+++ client/src/components/Layout/Navbar.js
@@ -1,49 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import LocalShippingIcon from '@material-ui/icons/LocalShipping';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+import './navbar.scss';
 
 export default function Navbar() {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
-      <AppBar position='static'>
-        <Toolbar>
-          <IconButton
-            edge='start'
-            className={classes.menuButton}
-            color='inherit'
-            aria-label='menu'
-          >
-            <LocalShippingIcon />
-          </IconButton>
-          <Typography variant='h6' className={classes.title}>
-            NinjaDispatcher
-          </Typography>
+    <Router>
+      <nav className='navbar bg-dark'>
+        <h1>
+          <Link to='/'>Ninja Dispatcher</Link>
+        </h1>
+        <ul>
           <Link to='/login'>
-            <Button color='inherit'>Login</Button>
+            <li>Admin Login</li>
           </Link>
-        </Toolbar>
-      </AppBar>
-    </div>
+        </ul>
+      </nav>
+    </Router>
   );
 }
