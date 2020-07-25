31ff1f6342217e2b60f853fe8bbfaaaa79531312
Maribelle Leong <maribelle.leong@gmail.com>
2020-07-25 15:29:36 -0300

    removed unused code

--- client/src/components/Schedule/TimeList.js
+++ client/src/components/Schedule/TimeList.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
