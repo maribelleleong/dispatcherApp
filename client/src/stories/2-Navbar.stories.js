6ecdfb70016134f2fdbf3ce6af3f8cd6eb0b3be3
Maribelle Leong <maribelle.leong@gmail.com>
2020-07-25 00:43:45 -0300

    storybook for Navbar

--- client/src/stories/2-Navbar.stories.js
+++ client/src/stories/2-Navbar.stories.js
@@ -3,6 +3,6 @@ import React, { Fragment } from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
-import Navbar from '../components/Layout/Navbar';
+import Navbar from '../components/layout/Navbar';
 
 storiesOf('Navbar', module).add('Navbar', () => <Navbar />);
