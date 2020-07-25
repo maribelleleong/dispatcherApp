5dffc7efbad61912f63882d7c6c070ac488d58b4
Maribelle Leong <maribelle.leong@gmail.com>
2020-07-25 14:05:52 -0300

    added Schedule component to App

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Login from './components/Login';
+import Toolbar from './components/Toolbar/Toolbar';
+import Schedule from './components/Schedule/Schedule';
 
 import './App.scss';
 
@@ -10,6 +12,8 @@ function App() {
   return (
     <Router>
       <Navbar />
+      <Toolbar />
+      <Schedule />
       <Switch>
         <Route exact path='/login' component={Login} />
       </Switch>
