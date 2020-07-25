1ff7f543c6120e5fdd7811b3aa53ae4376a003c0
Maribelle Leong <maribelle.leong@gmail.com>
2020-07-25 14:11:04 -0300

    storybook for Schedule

--- client/src/stories/4-Schedule.stories.js
+++ client/src/stories/4-Schedule.stories.js
@@ -4,8 +4,8 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import Toolbar from '../components/Toolbar/Toolbar';
-import DayList from '../components/Schedule/DayList/DayList';
-import TimeList from '../components/Schedule/TimeList/TimeList';
+import DayList from '../components/Schedule/DayList';
+import TimeList from '../components/Schedule/TimeList';
 
 storiesOf('Schedule', module)
   .add('DayList', () => <DayList />)
