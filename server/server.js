98304d99d105c38516827a7cd9e93f6cb3fa2722
Maribelle Leong <maribelle.leong@gmail.com>
2020-07-25 15:44:44 -0300

    added GET route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,16 @@
 const express = require('express');
-
 const app = express();
+
+// import data
+const { tasks_list } = require('./__fixture__/tasks_list');
+
+app.get('/tasks.json', (req, res) => {
+  res.json(tasks_list);
+});
+
+app.get('/tasks', (req, res) => {
+  res.json(task_list);
+});
 
 const PORT = process.env.PORT || 5000;
 
