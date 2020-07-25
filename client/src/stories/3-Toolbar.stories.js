642217f6443811bfa6a9ab0e3afc22c1a8d89c8e
Maribelle Leong <maribelle.leong@gmail.com>
2020-07-25 10:43:23 -0300

    story for DriverSelection

---
+++ client/src/stories/3-Toolbar.stories.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import Toolbar from '../components/Toolbar/Toolbar';
+import DriverSelection from '../components/Toolbar/DriverSelection';
+
+const drivers = ['John Doe', 'Fierce Bob', 'Aaron Smith'];
+
+storiesOf('Toolbar', module).add('DriverSelection', () => (
+  <DriverSelection drivers={drivers} />
+));
