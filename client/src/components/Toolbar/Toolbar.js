5d59025ef6a5bb61626d9fae1d7b477eec988054
Maribelle Leong <maribelle.leong@gmail.com>
2020-07-25 12:05:39 -0300

    added WeekSelection component and dsplayed with Grid

--- client/src/components/Toolbar/Toolbar.js
+++ client/src/components/Toolbar/Toolbar.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import DriverSelection from './DriverSelection';
+import { WeekSelection } from './WeekSelection';
 
-export default function Toolbar() {
-  return <div></div>;
-}
+import Grid from '@material-ui/core/Grid';
+
+const Toolbar = () => {
+  const drivers = ['John Doe', 'Fierce Bob', 'Aaron Smith'];
+
+  return (
+    <Grid
+      container
+      direction='row'
+      alignItems='flex-end'
+      justify='space-between'
+    >
+      <DriverSelection drivers={drivers} />
+      <WeekSelection />
+    </Grid>
+  );
+};
+
+export default Toolbar;
