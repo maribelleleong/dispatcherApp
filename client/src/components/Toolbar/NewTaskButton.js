fc582e9f216a0056b9e32c8741e63eeac1782cb9
Maribelle Leong <maribelle.leong@gmail.com>
2020-07-27 22:00:26 -0300

    refactored code

--- client/src/components/Toolbar/NewTaskButton.js
+++ client/src/components/Toolbar/NewTaskButton.js
@@ -1,25 +1,9 @@
 import React, { useState } from 'react';
-import styles from './styles';
+import NewTaskForm from './NewTaskForm';
 
 import axios from 'axios';
 import Modal from '@material-ui/core/Modal';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import MenuItem from '@material-ui/core/MenuItem';
-import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
-
-import { dayObj, jobTypes } from '../../helpers/constantObj';
-
-function getModalStyle() {
-  return {
-    top: `50%`,
-    left: `50%`,
-    transform: `translate(-50%, -50%)`,
-  };
-}
-
-const useStyles = makeStyles(styles);
 
 const NewTaskButton = ({
   drivers,
@@ -38,10 +22,6 @@ const NewTaskButton = ({
     type: '',
   });
   const [error, setError] = useState(false);
-
-  const classes = useStyles();
-  // getModalStyle is not a pure function, we roll the style only on the first render
-  const [modalStyle] = useState(getModalStyle);
   const [open, setOpen] = useState(false);
 
   const handleOpen = () => {
@@ -124,150 +104,19 @@ const NewTaskButton = ({
   };
 
   const body = (
-    <div style={modalStyle} className={classes.formPaper}>
-      <h2 className={classes.titleMargin} id='simple-modal-title'>
-        Create Task Form
-      </h2>
-      <form onSubmit={handleSubmit}>
-        <TextField
-          required
-          id='driver'
-          select
-          label='Driver'
-          name='driver'
-          value={state.driver}
-          onChange={changeInput}
-          className={`${classes.inputMargin} ${classes.largerWidth}`}
-        >
-          {drivers.map((driver) => (
-            <MenuItem key={driver} value={driver}>
-              {driver}
-            </MenuItem>
-          ))}
-        </TextField>
-        <br />
-        <TextField
-          required
-          className={`${classes.inputMargin} ${classes.smallerWidth}`}
-          id='week'
-          label='Week'
-          name='week'
-          type='number'
-          InputProps={{ inputProps: { min: 1, max: 52 } }}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          value={state.week}
-          onChange={changeInput}
-        />
-        <TextField
-          required
-          id='day'
-          select
-          label='Day'
-          name='day'
-          value={state.day}
-          onChange={changeInput}
-          className={`${classes.inputMargin} ${classes.largerWidth}`}
-        >
-          {Object.keys(dayObj).map((day) => (
-            <MenuItem key={day} value={dayObj[day]}>
-              {day}
-            </MenuItem>
-          ))}
-        </TextField>
-        <br />
-        <TextField
-          required
-          className={`${classes.inputMargin} ${classes.smallerWidth}`}
-          id='startTime'
-          name='startTime'
-          label='Start Time'
-          type='number'
-          InputProps={{ inputProps: { min: 0, max: 23 } }}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          value={state.startTime}
-          onChange={changeInput}
-        />
-        <TextField
-          required
-          className={`${classes.inputMargin} ${classes.smallerWidth}`}
-          id='end-time'
-          name='endTime'
-          label='Start Time'
-          type='number'
-          InputProps={{ inputProps: { min: 1, max: 24 } }}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          value={state.endTime}
-          onChange={changeInput}
-        />
-        <br />
-        <TextField
-          required
-          id='job-type'
-          select
-          label='Job Type'
-          name='type'
-          value={state.type}
-          onChange={changeInput}
-          className={`${classes.inputMargin} ${classes.smallerWidth}`}
-        >
-          {jobTypes.map((option) => (
-            <MenuItem key={option} value={option}>
-              {option}
-            </MenuItem>
-          ))}
-        </TextField>
-        <br />
-        <TextField
-          required
-          name='location'
-          id='location'
-          label='Location'
-          onChange={changeInput}
-          className={classes.inputMargin}
-          value={state.location}
-        />
-        <br />
-        {!error && (
-          <Button
-            color='primary'
-            variant='outlined'
-            className={classes.inputMargin}
-            type='submit'
-          >
-            Submit
-          </Button>
-        )}
-        {error && (
-          <>
-            <Typography className={classes.inputMargin}>
-              New task has time conflicts with existing task(s). Would you like
-              to overwrite?{' '}
-            </Typography>
-            <Button
-              color='primary'
-              variant='outlined'
-              className={classes.inputMargin}
-              onClick={handleOverwrite}
-            >
-              Overwrite
-            </Button>
-            <Button
-              variant='outlined'
-              className={classes.inputMargin}
-              onClick={handleCancel}
-            >
-              Cancel
-            </Button>
-          </>
-        )}
-      </form>
-    </div>
+    <NewTaskForm
+      {...{
+        drivers,
+        driver,
+        changeInput,
+        handleSubmit,
+        handleOverwrite,
+        handleCancel,
+        error,
+        setError,
+        state,
+      }}
+    />
   );
 
   return (
---
+++ client/src/components/Toolbar/NewTaskForm.js
@@ -0,0 +1,183 @@
+import React, { useState } from 'react';
+import styles from './styles';
+
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+import { dayObj, jobTypes } from '../../helpers/constantObj';
+
+function getModalStyle() {
+  return {
+    top: `50%`,
+    left: `50%`,
+    transform: `translate(-50%, -50%)`,
+  };
+}
+
+const useStyles = makeStyles(styles);
+
+const NewTaskForm = ({
+  drivers,
+  driver,
+  changeInput,
+  handleSubmit,
+  handleOverwrite,
+  handleCancel,
+  error,
+  state,
+}) => {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+
+  return (
+    <div style={modalStyle} className={classes.formPaper}>
+      <h2 className={classes.titleMargin} id='simple-modal-title'>
+        Create Task Form
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          required
+          id='driver'
+          select
+          label='Driver'
+          name='driver'
+          value={state.driver}
+          onChange={changeInput}
+          className={`${classes.inputMargin} ${classes.largerWidth}`}
+        >
+          {drivers.map((driver) => (
+            <MenuItem key={driver} value={driver}>
+              {driver}
+            </MenuItem>
+          ))}
+        </TextField>
+        <br />
+        <TextField
+          required
+          className={`${classes.inputMargin} ${classes.smallerWidth}`}
+          id='week'
+          label='Week'
+          name='week'
+          type='number'
+          InputProps={{ inputProps: { min: 1, max: 52 } }}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          value={state.week}
+          onChange={changeInput}
+        />
+        <TextField
+          required
+          id='day'
+          select
+          label='Day'
+          name='day'
+          value={state.day}
+          onChange={changeInput}
+          className={`${classes.inputMargin} ${classes.largerWidth}`}
+        >
+          {Object.keys(dayObj).map((day) => (
+            <MenuItem key={day} value={dayObj[day]}>
+              {day}
+            </MenuItem>
+          ))}
+        </TextField>
+        <br />
+        <TextField
+          required
+          className={`${classes.inputMargin} ${classes.smallerWidth}`}
+          id='startTime'
+          name='startTime'
+          label='Start Time'
+          type='number'
+          InputProps={{ inputProps: { min: 0, max: 23 } }}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          value={state.startTime}
+          onChange={changeInput}
+        />
+        <TextField
+          required
+          className={`${classes.inputMargin} ${classes.smallerWidth}`}
+          id='end-time'
+          name='endTime'
+          label='Start Time'
+          type='number'
+          InputProps={{ inputProps: { min: 1, max: 24 } }}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          value={state.endTime}
+          onChange={changeInput}
+        />
+        <br />
+        <TextField
+          required
+          id='job-type'
+          select
+          label='Job Type'
+          name='type'
+          value={state.type}
+          onChange={changeInput}
+          className={`${classes.inputMargin} ${classes.smallerWidth}`}
+        >
+          {jobTypes.map((option) => (
+            <MenuItem key={option} value={option}>
+              {option}
+            </MenuItem>
+          ))}
+        </TextField>
+        <br />
+        <TextField
+          required
+          name='location'
+          id='location'
+          label='Location'
+          onChange={changeInput}
+          className={classes.inputMargin}
+          value={state.location}
+        />
+        <br />
+        {!error && (
+          <Button
+            color='primary'
+            variant='outlined'
+            className={classes.inputMargin}
+            type='submit'
+          >
+            Submit
+          </Button>
+        )}
+        {error && (
+          <>
+            <Typography className={classes.inputMargin}>
+              New task has time conflicts with existing task(s). Would you like
+              to overwrite?{' '}
+            </Typography>
+            <Button
+              color='primary'
+              variant='outlined'
+              className={classes.inputMargin}
+              onClick={handleOverwrite}
+            >
+              Overwrite
+            </Button>
+            <Button
+              variant='outlined'
+              className={classes.inputMargin}
+              onClick={handleCancel}
+            >
+              Cancel
+            </Button>
+          </>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default NewTaskForm;
